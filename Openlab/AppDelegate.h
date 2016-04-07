4db6eef06afeb5e0f1cd617f33084ad4ce8a2f6f
yuyang <623240480@qq.com>
2016-04-07 13:08:36 +0800

    Initial Commit

---
+++ Openlab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Openlab
+//
+//  Created by admin on 16/4/7.
+//  Copyright © 2016年 cn.lztech  合肥联正电子科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
